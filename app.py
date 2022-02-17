c15d2b1c11b6ec78b9f3045d8fc4bad6160154a0
smithylee81 <smithylee81@yahoo.co.uk>
2022-02-17 11:46:07 +0000

    Update task into the database

--- app.py
+++ app.py
@@ -114,13 +114,28 @@ def add_task():
         mongo.db.tasks.insert_one(task)
         flash("Task Successfully Added")
         return redirect(url_for("get_tasks"))
-        
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
 
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "$set": {
+                "category_name": request.form.get("category_name"),
+                "task_name": request.form.get("task_name"),
+                "task_description": request.form.get("task_description"),
+                "is_urgent": is_urgent,
+                "due_date": request.form.get("due_date"),
+                "created_by": session["user"]
+            }
+        }
+        mongo.db.tasks.update_one({"_id": ObjectId(task_id)}, submit)
+        flash("Task Successfully Updated")
+
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
@@ -130,3 +145,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+            
